2bde506e81ed0922288e9e6c438c59e3ad7077bb
TaYaKi71751 <shantian.tw@gmail.com>
2023-05-22 13:18:35 +0900

    Add `--exit-node-allow-lan-access` option to `tailscale up`

--- sh/tailscale/up.sh
+++ sh/tailscale/up.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 	sudo ps -A | grep tailscaled && \
-	sudo tailscale up --qr --operator=$HOSTNAME --ssh --exit-node=$TAILSCALE_EXIT_NODE || \
+	sudo tailscale up --qr --operator=$HOSTNAME --ssh --exit-node=$TAILSCALE_EXIT_NODE --exit-node-allow-lan-access || \
 	(sudo systemd-run tailscaled && \
-	sudo tailscale up --qr --operator=$HOSTNAME --ssh --exit-node=$TAILSCALE_EXIT_NODE)
+	sudo tailscale up --qr --operator=$HOSTNAME --ssh --exit-node=$TAILSCALE_EXIT_NODE --exit-node-allow-lan-access)
 
