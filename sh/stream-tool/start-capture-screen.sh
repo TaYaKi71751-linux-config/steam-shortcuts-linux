b42e835045f4a2a56d47b7185d3e0eade6b6f9a7
TaYaKi71751 <shantian.tw@gmail.com>
2023-05-22 20:21:41 +0900

    Use nohup

--- sh/stream-tool/start-capture-screen.sh
+++ sh/stream-tool/start-capture-screen.sh
@@ -12,7 +12,7 @@ if [ "${XDG_SESSION_TYPE}" == "x11" ];then
 else
 	SOURCE="pipewiresrc"
 fi
-gst-launch-1.0 -e \
+nohup gst-launch-1.0 -e \
     ${SOURCE} do-timestamp=True \
         ! queue \
         ! videoconvert \
@@ -25,10 +25,12 @@ gst-launch-1.0 -e \
         ! fdkaacenc bitrate=256000 \
         ! mux. \
     flvmux name=mux streamable=True \
-        ! rtmpsink location='rtmp://localhost/stream/gstreamer live=1' & echo $! >> /tmp/screen-copy-gstreamer.pid \ # https://serverfault.com/questions/205498/how-to-get-pid-of-just-started-process
+				! rtmpsink location='rtmp://localhost/stream/gstreamer live=1' \
+				>/dev/null 2>&1 \
 &
 while [ true ];do
-sudo ffmpeg -f kmsgrab -i - -vaapi_device /dev/dri/renderD128 \
+nohup sudo ffmpeg -f kmsgrab -i - -vaapi_device /dev/dri/renderD128 \
 -vf hwmap=derive_device=vaapi,scale_vaapi=format=nv12 -c:v h264_vaapi -bf 1 \
--f flv rtmp://localhost/stream/ffmpeg & echo $! > /tmp/screen-copy-ffmpeg.pid # https://serverfault.com/questions/205498/how-to-get-pid-of-just-started-process
+-f flv rtmp://localhost/stream/ffmpeg \
+>/dev/null 2>&1
 done
