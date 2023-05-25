334bfeab2c1fc5c3c4297fecdb08b48204d8d6c4
Steam Deck User <deck@steamdeck.tailc1b5a.ts.net>
2023-05-25 11:56:25 +0900

    Fix `up.sh` with option `--reset`

--- sh/tailscale/up.sh
+++ sh/tailscale/up.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
-	sudo ps -A | grep tailscaled && \
-	sudo tailscale up --qr --operator=$HOSTNAME --ssh --exit-node=$TAILSCALE_EXIT_NODE --exit-node-allow-lan-access || \
-	(sudo systemd-run tailscaled && \
-	sudo tailscale up --qr --operator=$HOSTNAME --ssh --exit-node=$TAILSCALE_EXIT_NODE --exit-node-allow-lan-access)
+TAILSCALE_OPTIONS="${TAILSCALE_OPTIONS} --reset "
+TAILSCLAE_OPTIONS="${TAILSCLAE_OPTIONS} --exit-node=${TAILSCALE_EXIT_NODE} "
+TAILSCLAE_OPTIONS="${TAILSCLAE_OPTIONS} --ssh "
+TAILSCLAE_OPTIONS="${TAILSCLAE_OPTIONS} --operator=${HOSTNAME} "
+if [ -n "${TAILSCALE_EXIT_NODE}" ];then
+	TAILSCLAE_OPTIONS="${TAILSCLAE_OPTIONS} --exit-node-allow-lan-access "
+fi
 
+
+function up(){
+	DAEMON_STATUS=`sudo ps -A | grep tailscaled`
+	if [ -n "${DAEMON_STATUS}" ];then
+		echo ${TAILSCLAE_OPTIONS}
+		sudo tailscale up ${TAILSCLAE_OPTIONS}
+	else
+		sudo systemd-run tailscaled
+		up
+	fi
+}
+up
