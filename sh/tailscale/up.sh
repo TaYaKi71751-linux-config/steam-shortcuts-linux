1ae79bdbef990a75ddd58349e2029e35f9f51ec0
Steam Deck User <deck@steamdeck.tailc1b5a.ts.net>
2023-05-25 12:02:22 +0900

    Fix typo in `up.sh`

--- sh/tailscale/up.sh
+++ sh/tailscale/up.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 TAILSCALE_OPTIONS="${TAILSCALE_OPTIONS} --reset "
-TAILSCLAE_OPTIONS="${TAILSCLAE_OPTIONS} --exit-node=${TAILSCALE_EXIT_NODE} "
-TAILSCLAE_OPTIONS="${TAILSCLAE_OPTIONS} --ssh "
-TAILSCLAE_OPTIONS="${TAILSCLAE_OPTIONS} --operator=${HOSTNAME} "
+TAILSCALE_OPTIONS="${TAILSCALE_OPTIONS} --exit-node=${TAILSCALE_EXIT_NODE} "
+TAILSCALE_OPTIONS="${TAILSCALE_OPTIONS} --ssh "
+TAILSCALE_OPTIONS="${TAILSCALE_OPTIONS} --operator=${HOSTNAME} "
 if [ -n "${TAILSCALE_EXIT_NODE}" ];then
-	TAILSCLAE_OPTIONS="${TAILSCLAE_OPTIONS} --exit-node-allow-lan-access "
+	TAILSCALE_OPTIONS="${TAILSCALE_OPTIONS} --exit-node-allow-lan-access "
 fi
 
 
 function up(){
 	DAEMON_STATUS=`sudo ps -A | grep tailscaled`
 	if [ -n "${DAEMON_STATUS}" ];then
-		echo ${TAILSCLAE_OPTIONS}
-		sudo tailscale up ${TAILSCLAE_OPTIONS}
+		echo ${TAILSCALE_OPTIONS}
+		sudo tailscale up ${TAILSCALE_OPTIONS}
 	else
 		sudo systemd-run tailscaled
 		up
