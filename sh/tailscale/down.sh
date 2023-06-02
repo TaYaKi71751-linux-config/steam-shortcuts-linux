fe56c61be297e26caad6f0c824f85e84fc38b1bf
TaYaKi71751 <shantian.tw@gmail.com>
2023-06-02 09:57:18 +0900

    Add source shell_commands at sh/tailscale/*.sh

--- sh/tailscale/down.sh
+++ sh/tailscale/down.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+SHELL_RUN_COMMANDS=`find ~ -maxdepth 1 -name '.*shrc'`
+for shrc in ${SHELL_RUN_COMMANDS[@]};do
+	echo "source ${shrc}"
+	source ${shrc}
+done
+
 function down(){
 	DAEMON_STATUS=`sudo ps -A | grep tailscaled`
 	if [ -n "${DAEMON_STATUS}" ];then
+		echo "tailscaled already running"
 		sudo tailscale down
 	else
+		echo "tailscaled not running, run tailscaled"
 		sudo systemd-run tailscaled
 		down
 	fi
--- sh/tailscale/up.sh
+++ sh/tailscale/up.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+SHELL_RUN_COMMANDS=`find ~ -maxdepth 1 -name '.*shrc'`
+for shrc in ${SHELL_RUN_COMMANDS[@]};do
+	echo "source ${shrc}"
+	source ${shrc}
+done
+
 TAILSCALE_OPTIONS="${TAILSCALE_OPTIONS} --reset "
 TAILSCALE_OPTIONS="${TAILSCALE_OPTIONS} --exit-node=${TAILSCALE_EXIT_NODE} "
 TAILSCALE_OPTIONS="${TAILSCALE_OPTIONS} --ssh "
@@ -12,9 +18,11 @@ fi
 function up(){
 	DAEMON_STATUS=`sudo ps -A | grep tailscaled`
 	if [ -n "${DAEMON_STATUS}" ];then
+		echo "tailscaled already running"
 		echo ${TAILSCALE_OPTIONS}
 		sudo tailscale up ${TAILSCALE_OPTIONS}
 	else
+		echo "tailscaled not running, run tailscaled"
 		sudo systemd-run tailscaled
 		up
 	fi
