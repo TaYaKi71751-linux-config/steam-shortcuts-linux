df1e81eea2978a1fb56cc07575996c5570859644
TaYaKi71751 <shantian.tw@gmail.com>
2023-09-09 03:32:59 +0900

    Fix Update

--- add/update.ts
+++ add/update.ts
@@ -5,13 +5,13 @@ import { AddShortcut } from '../util/Shortcut';
 const outPath = path.join(
 	`${process.env.PWD}`,
 	'out',
-	'tailscale'
+	'update'
 );
 
 const outFiles = fs.readdirSync(outPath);
 outFiles
 	.forEach((filename) => {
-		const StartDir = outPath;
+		const StartDir = `${process.cwd()}`;
 		const exe = path.join(outPath, filename);
 		const AppName = 'Update TaYaKi71751/steam-shortcuts';
 		AddShortcut({ AppName, exe, StartDir });
