d1610b59b3dced27220695a89875713ab21d7f4b
TaYaKi71751 <shantian.tw@gmail.com>
2023-09-08 21:23:29 +0900

    Fix AppName of add/chome.ts

--- add/chrome.ts
+++ add/chrome.ts
@@ -9,19 +9,19 @@ const outPath = path.join(
 );
 const PAGE_URLS = [
 	{ name: 'Twitch', url: 'https://twitch.tv' },
-	{ name: 'YouTube TV', url: 'https://youtube.com/tv' },
+	{ name: 'YouTube', url: 'https://youtube.com/tv' },
 	{ name: 'bilibili', url: 'https://bilibili.com' },
-	{ name: '[OTT]Coupang Play', url: 'https://coupangplay.com/profiles' },
-	{ name: '[OTT]Crunchyroll', url: 'https://crunchyroll.com' },
-	{ name: '[OTT]Netflix', url: 'https://netflix.com' },
-	{ name: '[OTT]Laftel', url: 'https://laftel.net/profile' },
-	{ name: '[OTT]Watcha', url: 'https://watcha.com/manage_profiles' },
-	{ name: '[OTT]Wavve', url: 'https://www.wavve.com' },
-	{ name: '[OTT]Tubi', url: 'https://www.tubitv.com' },
-	{ name: '[Dashboard]Twitter Analytics', url: 'https://analytics.twitter.com' },
-	{ name: '[Dashboard]Twitch Dashboard', url: 'https://dashboard.twitch.tv/stream-manager' },
-	{ name: '[Dashboard]YouTube Studio', url: 'https://studio.youtube.com' },
-	{ name: '[Social]Twitter', url: 'https://twitter.com' }
+	{ name: 'Coupang Play', url: 'https://coupangplay.com/profiles' },
+	{ name: 'Crunchyroll', url: 'https://crunchyroll.com' },
+	{ name: 'Netflix', url: 'https://netflix.com' },
+	{ name: 'Laftel', url: 'https://laftel.net/profile' },
+	{ name: 'Watcha', url: 'https://watcha.com/manage_profiles' },
+	{ name: 'Wavve', url: 'https://www.wavve.com' },
+	{ name: 'Tubi', url: 'https://www.tubitv.com' },
+	{ name: 'Twitter Analytics', url: 'https://analytics.twitter.com' },
+	{ name: 'Twitch Dashboard', url: 'https://dashboard.twitch.tv/stream-manager' },
+	{ name: 'YouTube Studio', url: 'https://studio.youtube.com' },
+	{ name: 'Twitter', url: 'https://twitter.com' }
 ];
 
 const outFiles = fs.readdirSync(outPath);
@@ -29,15 +29,10 @@ outFiles
 	.forEach((filename) => {
 		const StartDir = outPath;
 		const exe = path.join(outPath, filename);
-		const AppName = '[Flatpak][Chrome]' + (function () {
-			switch (filename) {
-			case 'kiosk.out': return '[kiosk]';
-			}
-		})();
 		if (filename == 'kiosk.out') {
 			PAGE_URLS
 				.forEach(({ name, url }) => {
-					AddShortcut({ AppName: AppName + name, exe, StartDir, LaunchOptions: `PAGE_URL="${url}" %command%` });
+					AddShortcut({ AppName: name, exe, StartDir, LaunchOptions: `PAGE_URL="${url}" %command%` });
 				});
 		}
 	});
