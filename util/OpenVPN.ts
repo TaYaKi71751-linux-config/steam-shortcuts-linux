98075556aafec8e8e810df8d9a9fe51358aa6683
TaYaKi71751 <shantian.tw@gmail.com>
2023-09-09 02:35:40 +0900

    Fix with `|| true`

--- util/OpenVPN.ts
+++ util/OpenVPN.ts
@@ -3,7 +3,7 @@ import { execSync } from 'child_process';
 export function getOpenVPNConfigs () {
 	let r:string = '';
 	try {
-		r = execSync('find / -type f -name \'*.ovpn\' 2> /dev/null')?.toString();
+		r = execSync('find / -type f -name \'*.ovpn\' 2> /dev/null || true')?.toString();
 	} catch (e:any) {
 		r = `${e?.stdout?.toString()}`;
 	}
