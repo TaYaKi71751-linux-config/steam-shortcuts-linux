c64ea38d195251df51958bd799d2686f1e325798
TaYaKi71751 <shantian.tw@gmail.com>
2023-09-09 03:12:06 +0900

    Add OpenVPN kill

--- add/openvpn.ts
+++ add/openvpn.ts
@@ -1,9 +1,16 @@
 import { basename, dirname } from 'path';
 import { AddShortcut } from '../util/Shortcut';
 import { getOpenVPNConfigs, getWhichOpenVPN } from '../util/OpenVPN';
+import { execSync } from 'child_process';
 
 const outFiles = getOpenVPNConfigs();
 
+(function () {
+	const StartDir = `${process.env.HOME}`;
+	const exe = `${execSync('which pkill')?.toString()}`;
+	const AppName = '[OpenVPN] Kill';
+	AddShortcut({ AppName, exe, StartDir, LaunchOptions: 'sudo %command% openvpn' });
+})();
 outFiles
 	.forEach((OpenVPNConfigPath) => {
 		const StartDir = dirname(OpenVPNConfigPath);
