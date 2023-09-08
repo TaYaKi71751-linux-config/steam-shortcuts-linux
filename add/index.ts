84ca4afd9579a301f82539358a7c4cd2e9965a38
TaYaKi71751 <shantian.tw@gmail.com>
2023-09-09 02:34:56 +0900

    Fix require loop

--- add/index.ts
+++ add/index.ts
@@ -1,15 +1,15 @@
 import fs from 'fs';
-import path, { parse } from 'path';
+import path, { basename, parse } from 'path';
 
 const tsDirectory = path.join(
 	`${process.env.PWD}`,
 	'add'
 );
-
 const tsFiles = fs.readdirSync(tsDirectory);
 
 tsFiles
 	.filter((filename) => (filename.endsWith('.ts')))
+	.filter((filename) => (!basename(filename).startsWith('index')))
 	.forEach((filename) => {
 		const tsFilePath = path.join(tsDirectory, parse(filename).name);
 		require(tsFilePath);
