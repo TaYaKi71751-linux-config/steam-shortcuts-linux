d8ac1cf5d253cb2dd2db6eec0e1dec2e4c45a916
TaYaKi71751 <shantian.tw@gmail.com>
2023-09-14 12:33:35 +0900

    Remove --arch option at flatpak run

--- sh/chrome/kiosk.sh
+++ sh/chrome/kiosk.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/usr/bin/flatpak "run" "--branch=stable" "--arch=x86_64" "--command=/app/bin/edge" "--file-forwarding" "com.microsoft.Edge" "@@u" "@@"  --window-size=1024,640 --force-device-scale-factor=1.25 --device-scale-factor=1.25 --kiosk "${PAGE_URL}"
+/usr/bin/flatpak "run" "--branch=stable" "--command=/app/bin/edge" "--file-forwarding" "com.microsoft.Edge" "@@u" "@@"  --window-size=1024,640 --force-device-scale-factor=1.25 --device-scale-factor=1.25 --kiosk "${PAGE_URL}"
