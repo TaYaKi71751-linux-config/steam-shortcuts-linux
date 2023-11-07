e8462c796e41ae93073ec35d5c9f9b9a0cebee2a
TaYaKi71751 <shantian.tw@gmail.com>
2023-11-07 17:53:14 +0900

    Remove iptables-solo-lobby

--- sh/iptables-solo-lobby/temporarily-solo-lobby.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-SHELL_RUN_COMMANDS=`find ~ -maxdepth 1 -name '.*shrc'`
-for shrc in ${SHELL_RUN_COMMANDS[@]};do
-	source ${shrc}
-done
-
-source ./enable-solo-lobby.sh
-source ./disable-solo-lobby.sh
