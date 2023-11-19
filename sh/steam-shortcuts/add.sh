bfbd6543c9930eaebe24335cadbfb05c215fa4f8
TaYaKi71751 <shantian.tw@gmail.com>
2023-11-19 14:51:26 +0900

    Use both of kill -9 pkill

--- sh/steam-shortcuts/add.sh
+++ sh/steam-shortcuts/add.sh
@@ -5,7 +5,7 @@ for shrc in ${SHELL_RUN_COMMANDS[@]};do
 	echo "source ${shrc}"
 	source ${shrc}
 done
-
 find / -name 'pnpm' -type f -exec bash -c "{} i && pkill find" \;
 find / -name 'pnpm' -type f -exec bash -c "{} add:steam && pkill find" \;
-
+kill -9 $(pgrep steam)
+pkill steam
