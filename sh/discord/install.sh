27e0de1c5014a4cbcd3d61a1ee26bd848cddd4fb
TaYaKi71751 <shantian.tw@gmail.com>
2024-02-19 15:52:17 +0900

    Add Install Discord

---
+++ sh/discord/install.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+function user_install_flatpak_package(){
+	PACKAGE_NAME="$1"
+RESULT="$(bash << EOF
+flatpak install --user ${PACKAGE_NAME} --assumeyes
+EOF
+)"
+if ( echo ${RESULT} | grep already\ installed );then
+	echo A
+	return
+elif ( echo ${RESULT} | grep ${PACKAGE_NAME} | grep Similar );then
+	PACKAGE_LINES="$(echo "${RESULT}" | grep "${PACKAGE_NAME}/")"
+while IFS= read -r line
+do
+bash << EOF
+	flatpak install --user $(echo $line | rev | cut -d ' ' -f1 | rev) --assumeyes
+EOF
+done < <(printf '%s\n' "$PACKAGE_LINES")
+fi
+
+}
+
+function system_install_flatpak_package(){
+	PACKAGE_NAME="$1"
+RESULT="$(bash << EOF
+flatpak install --system ${PACKAGE_NAME} --assumeyes
+EOF
+)"
+if ( echo ${RESULT} | grep already\ installed );then
+	echo A
+	return
+elif ( echo ${RESULT} | grep ${PACKAGE_NAME} | grep Similar );then
+	PACKAGE_LINES="$(echo "${RESULT}" | grep "${PACKAGE_NAME}/")"
+while IFS= read -r line
+do
+bash << EOF
+	flatpak install --system $(echo $line | rev | cut -d ' ' -f1 | rev) --assumeyes
+EOF
+done < <(printf '%s\n' "$PACKAGE_LINES")
+fi
+
+}
+
+
+#Discord
+system_install_flatpak_package com.discordapp.Discord
+user_install_flatpak_package com.discordapp.Discord
