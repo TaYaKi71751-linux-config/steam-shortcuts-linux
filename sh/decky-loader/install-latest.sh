19f9f61d3763cd79ddf95deab12a88b8551599c3
TaYaKi71751 <shantian.tw@gmail.com>
2024-03-01 08:55:43 +0900

    Fix decky-loader install/uninstall

--- sh/decky-loader/install-latest.sh
+++ sh/decky-loader/install-latest.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-bash -c "$(curl -L https://github.com/SteamDeckHomebrew/decky-installer/releases/latest/download/install_release.sh)"
+curl -L https://github.com/SteamDeckHomebrew/decky-installer/releases/latest/download/install_release.sh | sh
--- sh/decky-loader/uninstall-latest.sh
+++ sh/decky-loader/uninstall-latest.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-bash -c "$(curl -L https://github.com/SteamDeckHomebrew/decky-installer/releases/latest/download/uninstall.sh)"
+curl -L https://github.com/SteamDeckHomebrew/decky-installer/releases/latest/download/uninstall.sh | sh
