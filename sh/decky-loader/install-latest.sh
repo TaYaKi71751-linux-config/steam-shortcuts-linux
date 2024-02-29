0b71246818270a2b33956d33889361b657c92628
TaYaKi71751 <shantian.tw@gmail.com>
2024-03-01 08:58:47 +0900

    Add pkill steam to decky-loader install uninstall

--- sh/decky-loader/install-latest.sh
+++ sh/decky-loader/install-latest.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 curl -L https://github.com/SteamDeckHomebrew/decky-installer/releases/latest/download/install_release.sh | sh
+pkill -9 steam
+pkill steam
--- sh/decky-loader/uninstall-latest.sh
+++ sh/decky-loader/uninstall-latest.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 curl -L https://github.com/SteamDeckHomebrew/decky-installer/releases/latest/download/uninstall.sh | sh
+pkill -9 steam
+pkill steam
