8c5c6a110757393a285079e6a365f0c2b8180b82
TaYaKi71751 <shantian.tw@gmail.com>
2024-03-02 18:44:04 +0900

    Add lutris

---
+++ add/lutris.ts
@@ -0,0 +1,70 @@
+import path from 'path';
+import fs from 'fs';
+import { AddShortcut } from '../util/Shortcut';
+import { getShortcutAppID } from '../util/AppID';
+import { AddToCats } from '../util/Categories';
+import { setBackground, setCapsule, setLogo, setWideCapsule } from '../util/Grid';
+
+const outPath = path.join(
+	`${process.env.PWD}`,
+	'out',
+	'lutris'
+);
+const DEEPLINK_URLS = [
+	{ name: 'Genshin Impact', url: 'lutris:genshin-impact-standard', tags: ['Genshin Impact', 'Lutris'] },
+	{ name: 'NexonPlug', url: 'lutris:nexonplug-standard-kr-region', tags: ['Nexon', 'Lutris'] },
+	{ name: 'Grand Theft Auto V (EGS)', url: 'lutris:grand-theft-auto-v-epic-games-launcher', tags: ['Grand Theft Auto V', 'Lutris'] },
+	{ name: 'Grand Theft Auto V (Rockstar)', url: 'lutris:grand-theft-auto-v-rockstar-games-launc', tags: ['Grand Theft Auto V', 'Lutris'] },
+	{ name: 'Grand Theft Auto V (PS3)', url: 'lutris:grand-theft-auto-v-ps3', tags: ['Grand Theft Auto V', 'Lutris'] },
+	{ name: 'Lutris', url: '', tags: ['Lutris'] }
+];
+
+export async function __main__ () {
+	const outFiles = fs.readdirSync(outPath);
+	for (let i = 0; i < outFiles?.length; i++) {
+		const filename = outFiles[i];
+		const StartDir = outPath;
+		const exe = path.join(outPath, filename);
+		if (filename == 'url.out') {
+			for (let j = 0; j < DEEPLINK_URLS?.length; j++) {
+				const { name, url, tags, icon, background, widecapsule, logo, capsule }:any = DEEPLINK_URLS[j];
+				const appid = getShortcutAppID({ AppName: name, exe });
+				AddShortcut({ appid, AppName: name, exe, StartDir, LaunchOptions: `LUTRIS_DEEPLINK_URL="${url}" %command%`, tags, icon: icon ?? '' });
+				if (background) {
+					setBackground({
+						appid,
+						path: background
+					});
+				}
+				if (widecapsule) {
+					setWideCapsule({
+						appid,
+						path: widecapsule
+					});
+				}
+				if (capsule) {
+					setCapsule({
+						appid,
+						path: capsule
+					});
+				}
+				if (logo) {
+					setLogo({
+						appid,
+						path: logo
+					});
+				}
+				for (let k = 0; k < tags?.length; k++) {
+					const tag = tags[k];
+					if (!tag) continue;
+					await AddToCats(appid, tag);
+				}
+			}
+		}
+	}
+}
+
+// https://stackoverflow.com/questions/4981891/node-js-equivalent-of-pythons-if-name-main
+if (typeof require !== 'undefined' && require.main === module) {
+	__main__();
+}
--- sh/lutris/url.sh
+++ sh/lutris/url.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 
-"flatpak" "run" "net.lutris.Lutris" "${LUTRIS_DEEPLINK_URL}"
+mkdir -p $HOME/.var/app/net.lutris.Lutris/cache/lutris/installer/
+
+SHELL_RUN_COMMANDS=`find ${ORIG_HOME} -maxdepth 1 -name '.*shrc'`
+for shrc in ${SHELL_RUN_COMMANDS[@]};do
+	echo "source ${shrc}"
+	source ${shrc}
+done
+
+function auto_path() {
+	TARGET_PATHS="$(find / -name "$1" -type f)"
+	echo $TARGET_PATHS
+	while IFS= read -r line
+	do
+		export PATH=${PATH}:$(dirname ${line})
+	done < <(printf '%s\n' "$TARGET_PATHS")
+}
+auto_path wine
+
+if [ -z "${LUTRIS_DEEPLINK_URL}" ];then
+	"flatpak" "run" "net.lutris.Lutris"
+else
+	"flatpak" "run" "net.lutris.Lutris" "${LUTRIS_DEEPLINK_URL}"
+fi
