85e58ac16285b97775931a19ea5c9a3d29cb41e4
TaYaKi71751 <shantian.tw@gmail.com>
2024-03-08 09:08:10 +0900

    Add bin install as fallback

--- sh/tailscale/install.sh
+++ sh/tailscale/install.sh
@@ -4,9 +4,11 @@
 TAILSCALE_TARGET_VERSION="$(pacman -Ss tailscale | grep /tailscale | rev | cut -d ' ' -f1 | rev | cut -d '-' -f1 )"
 echo ${TAILSCALE_TARGET_VERSION}
 
+which tailscale && exit
+
 #tailscale
-mkdir -p ~/.local/tailscale/steamos
-cd ~/.local/tailscale/steamos
+mkdir -p ~/.local/tailscale/git
+cd ~/.local/tailscale/git
 mkdir -p tailscale
 cd tailscale
 git init
@@ -18,6 +20,18 @@ git remote update
 git checkout v${TAILSCALE_TARGET_VERSION}
 ./build_dist.sh tailscale.com/cmd/tailscale
 ./build_dist.sh tailscale.com/cmd/tailscaled
+touch ~/.bashrc
+find ~/ -type f -name '.*shrc' -maxdepth 1 -exec sh -c 'grep -w "export PATH=\${PATH}:$(pwd)$" {} || echo "export PATH=\${PATH}:$(pwd)" >> {}' \;
+export PATH=${PATH}:$(pwd)
+
+which tailscale && exit
+uname -a | grep x86_64 || exit
+
+mkdir -p ~/.local/tailscale/bin
+cd ~/.local/tailscale/bin
+wget https://pkgs.tailscale.com/stable/tailscale_1.24.2_amd64.tgz
+tar xzf tailscale_1.24.2_amd64.tgz
+cd tailscale_1.24.2_amd64
 touch ~/.bashrc
 find ~/ -type f -name '.*shrc' -maxdepth 1 -exec sh -c 'grep -w "export PATH=\${PATH}:$(pwd)$" {} || echo "export PATH=\${PATH}:$(pwd)" >> {}' \;
 export PATH=${PATH}:$(pwd)
