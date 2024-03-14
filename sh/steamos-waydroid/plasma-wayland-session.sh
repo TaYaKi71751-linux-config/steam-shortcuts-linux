e63a5807f4746ed84bae78b5c387a832aa3689a0
TaYaKi71751 <shantian.tw@gmail.com>
2024-03-14 15:34:23 +0900

    Fix plasma-wayland-session.sh

--- sh/steamos-waydroid/plasma-wayland-session.sh
+++ sh/steamos-waydroid/plasma-wayland-session.sh
@@ -1,20 +1,22 @@
 #!/bin/sh
 # All Rights to /usr/bin/steamos-nested-desktop
+# Remove the performance overlay, it meddles with some tasks
 unset LD_PRELOAD
-
 CURRENT_WIDTH_HEIGHT="$(xrandr --current | grep connected | cut -d '+' -f1 | rev | cut -d ' ' -f1 | rev | tr -d '\n' | tr -d '\r')"
 
 WIDTH=`echo ${CURRENT_WIDTH_HEIGHT} | cut -d "x" -f1 | tr -d '\n' | tr -d '\r' | tr -d ' '`
 
 HEIGHT=`echo ${CURRENT_WIDTH_HEIGHT} | rev | cut -d "x" -f1 | rev | tr -d '\n' | tr -d '\r' | tr -d ' '`
 
-echo ${WIDTH} ${HEIGHT}
+## Shadow kwin_wayland_wrapper so that we can pass args to kwin wrapper
+## whilst being launched by plasma-session
 mkdir $XDG_RUNTIME_DIR/nested_plasma -p
 cat <<EOF > $XDG_RUNTIME_DIR/nested_plasma/kwin_wayland_wrapper
 #!/bin/sh
-/usr/bin/kwin_wayland_wrapper --width ${WIDTH} --height ${HEIGHT} --no-lockscreen \$@
+/usr/bin/kwin_wayland_wrapper --width $WIDTH --height $HEIGHT --no-lockscreen \$@
 EOF
-
+chmod a+x $XDG_RUNTIME_DIR/nested_plasma/kwin_wayland_wrapper
+export PATH=$XDG_RUNTIME_DIR/nested_plasma:$PATH
 
 dbus-run-session startplasma-wayland
 
