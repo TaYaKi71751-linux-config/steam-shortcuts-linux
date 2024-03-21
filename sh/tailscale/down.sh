a6c0fe5d20d0d3ed9f6e821a4dc69f2d0c99bb93
TaYaKi71751 <shantian.tw@gmail.com>
2024-03-21 23:00:36 +0900

    Fix path of tailscale

--- sh/tailscale/down.sh
+++ sh/tailscale/down.sh
@@ -30,6 +30,19 @@ function get_password(){
 	fi
 }
 
+function auto_path() {
+	TARGET_PATHS="$(find / -name "$1" -type f)"
+	echo $TARGET_PATHS
+	while IFS= read -r line
+	do
+		export PATH=${PATH}:$(dirname ${line})
+	done < <(printf '%s\n' "$TARGET_PATHS")
+}
+auto_path tailscaled
+auto_path tailscale
+
+which tailscaled || exit -1
+which tailscale || exit -1
 
 function check_sudo() {
 	if ( `sudo -nv` );then
--- sh/tailscale/up.sh
+++ sh/tailscale/up.sh
@@ -46,6 +46,21 @@ function check_sudo() {
 }
 check_sudo
 
+
+function auto_path() {
+	TARGET_PATHS="$(find / -name "$1" -type f)"
+	echo $TARGET_PATHS
+	while IFS= read -r line
+	do
+		export PATH=${PATH}:$(dirname ${line})
+	done < <(printf '%s\n' "$TARGET_PATHS")
+}
+auto_path tailscaled
+auto_path tailscale
+
+which tailscaled || exit -1
+which tailscale || exit -1
+
 # https://superuser.com/questions/553932/how-to-check-if-i-have-sudo-access
 function sudo_executor(){
 	if ( `sudo -nv` );then
