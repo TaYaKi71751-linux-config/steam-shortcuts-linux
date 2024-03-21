dd9fa26fd929990c79d13d9cc66dae17ff52fc37
TaYaKi71751 <shantian.tw@gmail.com>
2024-03-22 00:30:17 +0900

    Remove auto_path wine

--- sh/lutris/url.sh
+++ sh/lutris/url.sh
@@ -2,22 +2,6 @@
 
 mkdir -p $HOME/.var/app/net.lutris.Lutris/cache/lutris/installer/
 
-SHELL_RUN_COMMANDS=`find ${ORIG_HOME} -maxdepth 1 -name '.*shrc'`
-for shrc in ${SHELL_RUN_COMMANDS[@]};do
-	echo "source ${shrc}"
-	source ${shrc}
-done
-
-function auto_path() {
-	TARGET_PATHS="$(find / -name "$1" -type f)"
-	echo $TARGET_PATHS
-	while IFS= read -r line
-	do
-		export PATH=${PATH}:$(dirname ${line})
-	done < <(printf '%s\n' "$TARGET_PATHS")
-}
-auto_path wine
-
 if [ -z "${LUTRIS_DEEPLINK_URL}" ];then
 	"flatpak" "run" "net.lutris.Lutris"
 else
