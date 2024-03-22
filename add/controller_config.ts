ce3256db7740ba9810689d91fd07b81b5a93924c
TaYaKi71751 <shantian.tw@gmail.com>
2024-03-22 13:10:54 +0900

    Add __main__

--- add/controller_config.ts
+++ add/controller_config.ts
@@ -1,6 +1,12 @@
 import { execSync } from 'child_process';
 
-try {
-	const r = execSync(`cp ${process.env.PWD}/controller_config/*.vdf ${process.env.HOME}/.steam/steam/controller_base/templates/`).toString();
-	console.log(r);
-} catch (e) { console.error(e); }
+export async function __main__ () {
+	try {
+		const r = execSync(`cp ${process.env.PWD}/controller_config/*.vdf ${process.env.HOME}/.steam/steam/controller_base/templates/`).toString();
+		console.log(r);
+	} catch (e) { console.error(e); }
+}
+
+if (typeof require !== 'undefined' && require.main === module) {
+	__main__();
+}
