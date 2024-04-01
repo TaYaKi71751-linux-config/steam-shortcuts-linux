bc15d294ecb37505047e627d74e09b39742e00b8
TaYaKi71751 <shantian.tw@gmail.com>
2024-04-01 22:34:59 +0900

    Fix prefix

--- sh/3dmigoto/run.sh
+++ sh/3dmigoto/run.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
-mkdir -p "$HOME/Games/genshin-impact/prefix/drive_c/"
+__GAME_NAME__="genshin-impact"
 __GI_PATH__="$(find / -name 'GenshinImpact.exe' -type f | tail -n 1)"
 echo $__GI_PATH__
-ln -sf "$(dirname "$__GI_PATH__")" "$HOME/Games/genshin-impact/prefix/drive_c/"
+ln -sf "$(dirname "$__GI_PATH__")" "$HOME/Games/${__GAME_NAME__}/drive_c/"
 __3DMIGOTO_PATH__="$(find $HOME/ -name '3DMigoto Loader.exe' | tail -n 1)"
 echo $__3DMIGOTO_PATH__
-ln -sf "$(dirname "$__3DMIGOTO_PATH__")" "$HOME/Games/genshin-impact/prefix/drive_c/"
+ln -sf "$(dirname "$__3DMIGOTO_PATH__")" "$HOME/Games/${__GAME_NAME__}/drive_c/"
 
 GI_CONF_PATHS="$(find $HOME/.var/app/net.lutris.Lutris/data/lutris/games/ -name 'genshin-impact-*.yml')"
+GI_CONF_PATHS="$(find $HOME/.var/app/net.lutris.Lutris/data/lutris/games/ -name "${__GAME_NAME__}-*.yml")"
 
 echo ${GI_CONF_PATHS}
 while IFS= read -r GI_CONF_PATH
 do
  echo $GI_CONF_PATH
 echo "game:" > $GI_CONF_PATH
-echo "  exe: $HOME/Games/genshin-impact/prefix/drive_c/GenshinImpact.bat" >> $GI_CONF_PATH
-echo "  prefix: $HOME/Games/genshin-impact/prefix/" >> $GI_CONF_PATH
+echo "  exe: $HOME/Games/genshin-impact/drive_c/GenshinImpact.bat" >> $GI_CONF_PATH
+echo "  prefix: $HOME/Games/genshin-impact/" >> $GI_CONF_PATH
+echo "  exe: $HOME/Games/${__GAME_NAME__}/drive_c/GenshinImpact.bat" >> $GI_CONF_PATH
+echo "  prefix: $HOME/Games/${__GAME_NAME__}/" >> $GI_CONF_PATH
 echo "wine:" >> $GI_CONF_PATH
 echo "  battleye: false" >> $GI_CONF_PATH
 echo "  dxvk_nvapi: false" >> $GI_CONF_PATH
@@ -25,7 +28,7 @@ echo "  fsr: false" >> $GI_CONF_PATH
 echo "  vkd3d: false" >> $GI_CONF_PATH
 done < <(printf '%s\n' "${GI_CONF_PATHS}")
 
-export BATCH_PATH="${HOME}/Games/genshin-impact/prefix/drive_c/GenshinImpact.bat"
+export BATCH_PATH="${HOME}/Games/${__GAME_NAME__}/drive_c/GenshinImpact.bat"
 
 echo "cd C:\\3dmigoto" > $BATCH_PATH
 echo "start \"\" \"3DMigoto Loader.exe\"" >> $BATCH_PATH
