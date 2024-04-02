b79eb23a70df611ff11ca76f3d9d6be4610d2fa4
TaYaKi71751 <shantian.tw@gmail.com>
2024-04-02 12:22:56 +0900

    Merge AAGL.ts into GenshinImpact.ts

--- add/GenshinImpact.ts
+++ add/GenshinImpact.ts
@@ -72,6 +72,36 @@ export async function __main__ () {
 			}
 		}
 	}
+
+	// AAGL
+	{
+		let tags = ['Genshin Impact', 'AAGL'];
+
+		const outPath = path.join(`${process.env.PWD}`,'out','GI');
+		const outFiles = ['install_aagl.out','launch_aagl.out'];
+		for (let i = 0; i < outFiles?.length; i++) {
+			const filename = outFiles[i];
+			const StartDir = outPath;
+			const exe = path.join(outPath, filename);
+			const AppName = '[AAGL]' + (function () {
+				switch (filename) {
+					case 'install_aagl.out':
+						tags = ['Install', 'Genshin Impact', 'AAGL'];
+					return 'Install';
+					case 'launch_aagl.out':
+						tags = ['Genshin Impact', 'AAGL'];
+					return 'Genshin Impact Launcher';
+				}
+			})();
+			const appid = getShortcutAppID({ AppName, exe });
+			AddShortcut({ appid, AppName, exe, StartDir, LaunchOptions: '%command%', tags });
+			for (let j = 0; j < tags?.length; j++) {
+				const tag = tags[j];
+				if (!tag) continue;
+				await AddToCats(appid, tag);
+			}
+		}
+	}
 }
 
 // https://stackoverflow.com/questions/4981891/node-js-equivalent-of-pythons-if-name-main
