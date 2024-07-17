1da466a8bb153b552d6881cf55192c13e2af2b3f
TaYaKi71751 <shantian.tw@gmail.com>
2024-07-18 02:59:19 +0900

    Add ZZZ/*_lutris.sh

---
+++ sh/ZZZ/3dmigoto_lutris.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+__EXE_NAME__="ZenlessZoneZero.exe"
+
+__GAME_NAME__="zenless-zone-zero"
+__EXE_PATH__="$(find / -name "${__EXE_NAME__}" -type f | tail -n 1)"
+echo $__EXE_PATH__
+mkdir -p "$HOME/Games/${__GAME_NAME__}/drive_c/"
+ln -sf "$(dirname "$__EXE_PATH__")" "$HOME/Games/${__GAME_NAME__}/drive_c/"
+__D3DX_PATHS__="$(find $HOME/ -name 'd3dx.ini')"
+
+
+while IFS= read -r __D3DX_PATH__
+do
+	if ( cat $__D3DX_PATH__ | grep ${__EXE_NAME__} );then
+		export __3DMIGOTO_PATH__="$(dirname "$__D3DX_PATH__")/3DMigoto Loader.exe"
+	fi
+done < <(printf '%s\n' "$__D3DX_PATHS__")
+echo $__3DMIGOTO_PATH__
+ln -sf "$(dirname "$__3DMIGOTO_PATH__")" "$HOME/Games/${__GAME_NAME__}/drive_c/"
+
+# rm $HOME/.var/app/net.lutris.Lutris/data/lutris/pga.db
+sqlite3 $HOME/.var/app/net.lutris.Lutris/data/lutris/pga.db << EOF
+DELETE FROM games WHERE name = '${__GAME_NAME__}';
+.save $HOME/.var/app/net.lutris.Lutris/data/lutris/pga.db.tmp
+EOF
+mv $HOME/.var/app/net.lutris.Lutris/data/lutris/pga.db.tmp $HOME/.var/app/net.lutris.Lutris/data/lutris/pga.db
+
+if ( ls $HOME/.var/app/net.lutris.Lutris/data/lutris/games/genshin-impact-*.yml );then
+ rm $HOME/.var/app/net.lutris.Lutris/data/lutris/games/genshin-impact-*.yml
+fi
+
+if ( ls /usr/bin/obs-gamecapture );then
+cp /usr/bin/obs-gamecapture $HOME/.var/app/net.lutris.Lutris/data/lutris/runners/wine/
+cat > $HOME/.var/app/net.lutris.Lutris/data/lutris/games/newgame.yml << EOF
+name: ${__GAME_NAME__}
+game_slug: ${__GAME_NAME__}
+version: Installer
+slug: ${__GAME_NAME__}
+runner: wine
+script:
+  game:
+    exe: $HOME/Games/${__GAME_NAME__}/drive_c/launch.bat
+    prefix: $HOME/Games/${__GAME_NAME__}/
+  wine:
+    battleye: false
+    dxvk_nvapi: false
+    eac: false
+    fsr: false
+    vkd3d: false
+  system:
+    prefix_command: $HOME/.var/app/net.lutris.Lutris/data/lutris/runners/wine/obs-gamecapture
+EOF
+else
+cat > $HOME/.var/app/net.lutris.Lutris/data/lutris/games/newgame.yml << EOF
+name: ${__GAME_NAME__}
+game_slug: ${__GAME_NAME__}
+version: Installer
+slug: ${__GAME_NAME__}
+runner: wine
+script:
+  game:
+    exe: $HOME/Games/${__GAME_NAME__}/drive_c/launch.bat
+    prefix: $HOME/Games/${__GAME_NAME__}/
+  wine:
+    battleye: false
+    dxvk_nvapi: false
+    eac: false
+    fsr: false
+    vkd3d: false
+  system:
+    prefix_command: /home/deck/.var/app/net.lutris.Lutris/data/lutris/runners/wine/obs-gamecapture
+EOF
+fi
+export BATCH_PATH="${HOME}/Games/${__GAME_NAME__}/drive_c/launch.bat"
+
+echo "cd C:\\$(dirname "${__3DMIGOTO_PATH__}" | rev | cut -d '/' -f1 | rev)" > $BATCH_PATH
+echo "start \"\" \"3DMigoto Loader.exe\"" >> $BATCH_PATH
+echo "cd C:\\$(dirname "${__EXE_PATH__}" | rev | cut -d '/' -f1 | rev)" >> $BATCH_PATH
+echo "start \"\" ${__EXE_NAME__}" >> $BATCH_PATH
+flatpak run net.lutris.Lutris -i $HOME/.var/app/net.lutris.Lutris/data/lutris/games/newgame.yml
+
