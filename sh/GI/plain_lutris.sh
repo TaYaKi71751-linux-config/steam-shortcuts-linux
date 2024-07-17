b008fd5fa8bae65f37807fad936da8602abcdbac
TaYaKi71751 <shantian.tw@gmail.com>
2024-07-18 07:45:39 +0900

    Use lutris:rungameid

--- sh/GI/plain_lutris.sh
+++ sh/GI/plain_lutris.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
+
 __EXE_NAME__="GenshinImpact.exe"
+
 __GAME_NAME__="genshin-impact"
 __EXE_PATH__="$(find / -name "${__EXE_NAME__}" -type f | tail -n 1)"
 echo $__EXE_PATH__
-mkdir -p "$HOME/Games/${__GAME_NAME__}/drive_c/"
-ln -sf "$(dirname "$__EXE_PATH__")" "$HOME/Games/${__GAME_NAME__}/drive_c/"
-__3DMIGOTO_PATH__="$(find $HOME/ -name '3DMigoto Loader.exe' | tail -n 1)"
-echo $__3DMIGOTO_PATH__
-ln -sf "$(dirname "$__3DMIGOTO_PATH__")" "$HOME/Games/${__GAME_NAME__}/drive_c/"
 
 # rm $HOME/.var/app/net.lutris.Lutris/data/lutris/pga.db
 sqlite3 $HOME/.var/app/net.lutris.Lutris/data/lutris/pga.db << EOF
@@ -16,55 +13,109 @@ DELETE FROM games WHERE name = '${__GAME_NAME__}';
 EOF
 mv $HOME/.var/app/net.lutris.Lutris/data/lutris/pga.db.tmp $HOME/.var/app/net.lutris.Lutris/data/lutris/pga.db
 
+sqlite3 $HOME/.var/app/net.lutris.Lutris/data/lutris/pga.db << EOF
+INSERT INTO games (
+ name,
+	slug,
+	installer_slug,
+	platform,
+	runner,
+	configpath,
+	installed,
+	lastplayed,
+	installed_at,
+	has_custom_banner,
+	has_custom_icon,
+	has_custom_coverart_big,
+	playtime
+) VALUES (
+ '${__GAME_NAME__}',
+ '${__GAME_NAME__}',
+ '${__GAME_NAME__}',
+	'Windows',
+	'wine',
+	'${__GAME_NAME__}-0',
+	1,
+	0,
+	0,
+	0,
+	0,
+	0,
+	0.0
+);
+.save $HOME/.var/app/net.lutris.Lutris/data/lutris/pga.db.tmp
+EOF
+mv $HOME/.var/app/net.lutris.Lutris/data/lutris/pga.db.tmp $HOME/.var/app/net.lutris.Lutris/data/lutris/pga.db
+
+__GAME_ID__="$(sqlite3 $HOME/.var/app/net.lutris.Lutris/data/lutris/pga.db << EOF
+SELECT id FROM games WHERE slug = '${__GAME_NAME__}';
+EOF
+)"
+
 if ( ls $HOME/.var/app/net.lutris.Lutris/data/lutris/games/genshin-impact-*.yml );then
  rm $HOME/.var/app/net.lutris.Lutris/data/lutris/games/genshin-impact-*.yml
 fi
 
 if ( ls /usr/bin/obs-gamecapture );then
 cp /usr/bin/obs-gamecapture $HOME/.var/app/net.lutris.Lutris/data/lutris/runners/wine/
-cat > $HOME/.var/app/net.lutris.Lutris/data/lutris/games/newgame.yml << EOF
-name: ${__GAME_NAME__}
+cat > $HOME/.var/app/net.lutris.Lutris/data/lutris/games/${__GAME_NAME__}-0.yml << EOF
+game:
+  exe: $HOME/Games/${__GAME_NAME__}/drive_c/launch.bat
+  prefix: $HOME/Games/${__GAME_NAME__}/
 game_slug: ${__GAME_NAME__}
-version: Installer
-slug: ${__GAME_NAME__}
-runner: wine
+name: ${__GAME_NAME__}
 script:
   game:
     exe: $HOME/Games/${__GAME_NAME__}/drive_c/launch.bat
     prefix: $HOME/Games/${__GAME_NAME__}/
   wine:
     battleye: false
-    dxvk_nvapi: false
-    eac: false
+    dxvk_nvapi: true
+    eac: true
     fsr: false
     vkd3d: false
-  system:
-    prefix_command: $HOME/.var/app/net.lutris.Lutris/data/lutris/runners/wine/obs-gamecapture
+		system:
+    prefix_command: ${HOME}/.var/app/net.lutris.Lutris/data/lutris/runners/wine/obs-gamecapture
+slug: ${__GAME_NAME__}
+version: Installer
+wine:
+  battleye: false
+  dxvk_nvapi: true
+  eac: true
+  fsr: false
+  vkd3d: false
+system:
+  prefix_command: ${HOME}/.var/app/net.lutris.Lutris/data/lutris/runners/wine/obs-gamecapture
 EOF
 else
-cat > $HOME/.var/app/net.lutris.Lutris/data/lutris/games/newgame.yml << EOF
-name: ${__GAME_NAME__}
+cat > $HOME/.var/app/net.lutris.Lutris/data/lutris/games/${__GAME_NAME__}-0.yml << EOF
+game:
+  exe: $HOME/Games/${__GAME_NAME__}/drive_c/launch.bat
+  prefix: $HOME/Games/${__GAME_NAME__}/
 game_slug: ${__GAME_NAME__}
-version: Installer
-slug: ${__GAME_NAME__}
-runner: wine
+name: ${__GAME_NAME__}
 script:
   game:
     exe: $HOME/Games/${__GAME_NAME__}/drive_c/launch.bat
     prefix: $HOME/Games/${__GAME_NAME__}/
   wine:
     battleye: false
-    dxvk_nvapi: false
-    eac: false
+    dxvk_nvapi: true
+    eac: true
     fsr: false
     vkd3d: false
-  system:
-    prefix_command: /home/deck/.var/app/net.lutris.Lutris/data/lutris/runners/wine/obs-gamecapture
+slug: ${__GAME_NAME__}
+version: Installer
+wine:
+  battleye: false
+  dxvk_nvapi: true
+  eac: true
+  fsr: false
+  vkd3d: false
 EOF
 fi
 export BATCH_PATH="${HOME}/Games/${__GAME_NAME__}/drive_c/launch.bat"
 
-echo "cd C:\\$(dirname "${__EXE_PATH__}" | rev | cut -d '/' -f1 | rev)" > $BATCH_PATH
+echo "cd C:\\$(dirname "${__EXE_PATH__}" | rev | cut -d '/' -f1 | rev)" >> $BATCH_PATH
 echo "start \"\" ${__EXE_NAME__}" >> $BATCH_PATH
-flatpak run net.lutris.Lutris -i $HOME/.var/app/net.lutris.Lutris/data/lutris/games/newgame.yml
-
+flatpak run net.lutris.Lutris "lutris:rungameid/${__GAME_ID__}"
