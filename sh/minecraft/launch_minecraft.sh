a5c0477ac2dab5fb3a82d77b59bb9e90524d19c8
TaYaKi71751 <shantian.tw@gmail.com>
2024-07-27 16:03:21 +0900

    Use aur for minecraft-launcher

--- sh/minecraft/launch_minecraft.sh
+++ sh/minecraft/launch_minecraft.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-rm -rf $HOME/.var/app/com.mojang.Minecraft/.minecraft
-mkdir -p $HOME/.minecraft
-ln -sf $HOME/.minecraft $HOME/.var/app/com.mojang.Minecraft/
-
-flatpak run com.mojang.Minecraft
+minecraft-launcher
