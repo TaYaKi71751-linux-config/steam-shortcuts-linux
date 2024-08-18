5e56b16c0a4a54a3a4ef6be85b322c6c6580fb93
sh4nt14n <sh4nt14n@sh4nt14nui-MacBook.local>
2024-08-18 17:39:14 +0900

    Add launch_game.sh

---
+++ sh/WW/launch_game.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+__GAME_NAME__="WutheringWaves"
+__LAUNCHER_PACKAGE__="moe.launcher.wavey-launcher"
+__LAUNCHER_NAME__="wavey-launcher"
+
+function auto_path() {
+	TARGET_PATHS="$(find / -name "$1" -type f)"
+	echo $TARGET_PATHS
+	while IFS= read -r line
+	do
+		export PATH=${PATH}:$(dirname ${line})
+	done < <(printf '%s\n' "$TARGET_PATHS")
+}
+
+auto_path wine
+
+WINEPREFIX="${HOME}/WutheringWaves/prefix" wine "${HOME}/WutheringWaves/game/Wuthering Waves.exe"
