9924b65873cd4907bb4411c1c1b7afd8b5a018f6
sh4nt14n <sh4nt14n@sh4nt14nui-MacBook.local>
2024-08-18 16:08:30 +0900

    Fix wine path for vcrun

--- sh/WW/install_vcrun.sh
+++ sh/WW/install_vcrun.sh
@@ -4,6 +4,17 @@ __GAME_NAME__="WutheringWaves"
 __LAUNCHER_PACKAGE__="moe.launcher.wavey-launcher"
 __LAUNCHER_NAME__="wavey-launcher"
 
+function auto_path() {
+	TARGET_PATHS="$(find / -name "$1" -type f)"
+	echo $TARGET_PATHS
+	while IFS= read -r line
+	do
+		export PATH=${PATH}:$(dirname ${line})
+	done < <(printf '%s\n' "$TARGET_PATHS")
+}
+
+auto_path wine
+
 mkdir -p "${HOME}/${__GAME_NAME__}/vcrun"
 cd "${HOME}/${__GAME_NAME__}/vcrun"
 rm *.exe
