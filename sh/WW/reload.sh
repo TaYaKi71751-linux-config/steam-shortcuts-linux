babb61d47f5e93a281c3b29f5de74f10033d5cfd
sh4nt14n <sh4nt14n@sh4nt14nui-MacBook.local>
2024-08-18 16:45:31 +0900

    Add sh/WW/reload.sh

---
+++ sh/WW/reload.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+SHELL_RUN_COMMANDS=`find ${ORIG_HOME} -maxdepth 1 -name '.*shrc'`
+for shrc in ${SHELL_RUN_COMMANDS[@]};do
+	echo "source ${shrc}"
+	source ${shrc}
+done
+
+function auto_path() {
+	TARGET_PATHS="$(find / -name "$1" -type f)"
+	echo $TARGET_PATHS
+	while IFS= read -r line
+	do
+		export PATH=${PATH}:$(dirname ${line})
+	done < <(printf '%s\n' "$TARGET_PATHS")
+}
+auto_path node
+auto_path pnpm
+auto_path ts-node
+
+pnpm i
+ts-node ./add/WutheringWaves.ts
+kill -9 $(pgrep steam)
+pkill steam
