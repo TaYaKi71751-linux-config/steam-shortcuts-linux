99df7f16a321c87ca05591fa27167d4119d7a079
sh4nt14n <sh4nt14n@sh4nt14nui-MacBook.local>
2024-08-18 16:24:24 +0900

    Fix vcrun to 2019

--- sh/WW/install_vcrun.sh
+++ sh/WW/install_vcrun.sh
@@ -19,9 +19,9 @@ mkdir -p "${HOME}/${__GAME_NAME__}/vcrun"
 cd "${HOME}/${__GAME_NAME__}/vcrun"
 rm *.exe
 
-wget "https://download.microsoft.com/download/0/6/4/064F84EA-D1DB-4EAA-9A5C-CC2F0FF6A638/vc_redist.x64.exe"
-wget "https://download.microsoft.com/download/0/6/4/064F84EA-D1DB-4EAA-9A5C-CC2F0FF6A638/vc_redist.x86.exe"
+wget "https://aka.ms/vs/16/release/vc_redist.x86.exe"
+wget "https://aka.ms/vs/16/release/vc_redist.x64.exe"
 
 
-WINEPREFIX="${HOME}/${__GAME_NAME__}/prefix" wine "${HOME}/${__GAME_NAME__}/vcrun/vc_redist.x86.exe"
-WINEPREFIX="${HOME}/${__GAME_NAME__}/prefix" wine "${HOME}/${__GAME_NAME__}/vcrun/vc_redist.x64.exe"
+WINEPREFIX="${HOME}/${__GAME_NAME__}/prefix" wine "${HOME}/${__GAME_NAME__}/vcrun/VC_redist.x86.exe"
+WINEPREFIX="${HOME}/${__GAME_NAME__}/prefix" wine "${HOME}/${__GAME_NAME__}/vcrun/VC_redist.x64.exe"
