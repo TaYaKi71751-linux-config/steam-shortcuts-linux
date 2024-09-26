24e7c8afe58854d9ae2b09fd74b800b0e5d46f50
TaYaKi71751 <shantian.tw@gmail.com>
2024-09-26 18:51:21 +0900

    Add Battle.net

---
+++ add/BattleNet.ts
@@ -0,0 +1,43 @@
+import { execSync } from 'child_process'
+import { existsSync } from 'fs';
+import { getShortcutAppID } from '../util/AppID';
+import { AddShortcut, RemoveShortcutStartsWith } from '../util/Shortcut';
+import { AddToCats } from '../util/Categories';
+import { AddCompat } from '../util/Compatibilities';
+import path,{ dirname } from 'path';
+
+export async function __main__ () {
+	const __OUT_NAME__ = 'battlenet';
+
+	// Lutris
+	{
+		const tags = [__GAME_NAME__,'Lutris'];
+		const outPath = path.join(`${process.env.PWD}`, 'out', __OUT_NAME__);
+		const outFiles = fs.readdirSync(outPath);
+		for (let i = 0; i < outFiles?.length; i++) {
+			const filename = outFiles[i];
+			const StartDir = outPath;
+			const exe = path.join(outPath, filename);
+			const AppName = `[Lutris] ` + (() => {
+				switch(filename){
+					case 'install_battlenet.out': return 'Install Battle.net';
+					case 'launch_battlenet.out': return 'Battle.net';
+					case 'launch_d2r.out': return 'Diablo II Resurrected';
+				}
+			})();
+			const appid = getShortcutAppID({ AppName, exe });
+			AddShortcut({ appid, AppName, exe, StartDir });
+			for (let j = 0; j < tags?.length; j++) {
+				const tag = tags[j];
+				if (!tag) continue;
+				await AddToCats(appid, tag);
+			}
+		}
+	}
+
+}
+
+// https://stackoverflow.com/questions/4981891/node-js-equivalent-of-pythons-if-name-main
+if (typeof require !== 'undefined' && require.main === module) {
+	__main__();
+}
---
+++ sh/battlenet/launch_battlenet.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+
+mkdir -p ${HOME}/BattleNet/
+cd ${HOME}/BattleNet
+
+__GAME_NAME__="battlenet"
+
+mkdir -p "$HOME/Games/${__GAME_NAME__}/drive_c/"
+
+# rm $HOME/.var/app/net.lutris.Lutris/data/lutris/pga.db
+sqlite3 $HOME/.var/app/net.lutris.Lutris/data/lutris/pga.db << EOF
+DELETE FROM games WHERE name = '${__GAME_NAME__}';
+.save $HOME/.var/app/net.lutris.Lutris/data/lutris/pga.db.tmp
+EOF
+mv $HOME/.var/app/net.lutris.Lutris/data/lutris/pga.db.tmp $HOME/.var/app/net.lutris.Lutris/data/lutris/pga.db
+
+sqlite3 $HOME/.var/app/net.lutris.Lutris/data/lutris/pga.db << EOF
+INSERT INTO games (
+ name,
+	slug,
+	installer_slug,
+	platform,
+	runner,
+	configpath,
+	installed,
+	lastplayed,
+	installed_at,
+	has_custom_banner,
+	has_custom_icon,
+	has_custom_coverart_big,
+	playtime
+) VALUES (
+ '${__GAME_NAME__}',
+ '${__GAME_NAME__}',
+ '${__GAME_NAME__}',
+	'Windows',
+	'wine',
+	'${__GAME_NAME__}-0',
+	1,
+	0,
+	0,
+	0,
+	0,
+	0,
+	0.0
+);
+.save $HOME/.var/app/net.lutris.Lutris/data/lutris/pga.db.tmp
+EOF
+mv $HOME/.var/app/net.lutris.Lutris/data/lutris/pga.db.tmp $HOME/.var/app/net.lutris.Lutris/data/lutris/pga.db
+
+__GAME_ID__="$(sqlite3 $HOME/.var/app/net.lutris.Lutris/data/lutris/pga.db << EOF
+SELECT id FROM games WHERE slug = '${__GAME_NAME__}';
+EOF
+)"
+
+if ( ls $HOME/.var/app/net.lutris.Lutris/data/lutris/games/${__GAME_NAME__}-*.yml );then
+	rm $HOME/.var/app/net.lutris.Lutris/data/lutris/games/${__GAME_NAME__}-*.yml
+fi
+
+if ( ls /usr/bin/obs-gamecapture );then
+cp /usr/bin/obs-gamecapture $HOME/.var/app/net.lutris.Lutris/data/lutris/runners/wine/
+cat > $HOME/.var/app/net.lutris.Lutris/data/lutris/games/${__GAME_NAME__}-0.yml << EOF
+game:
+		exe: $HOME/Games/${__GAME_NAME__}/drive_c/Program Files (x86)/Battle.net/Battle.net Launcher.exe
+  prefix: $HOME/Games/${__GAME_NAME__}/
+game_slug: ${__GAME_NAME__}
+name: ${__GAME_NAME__}
+script:
+  game:
+    exe: $HOME/Games/${__GAME_NAME__}/drive_c/Program Files (x86)/Battle.net/Battle.net Launcher.exe
+    prefix: $HOME/Games/${__GAME_NAME__}/
+  wine:
+    battleye: true
+    dxvk_nvapi: false
+    eac: true
+    fsr: false
+    vkd3d: false
+  system:
+    prefix_command: ${HOME}/.var/app/net.lutris.Lutris/data/lutris/runners/wine/obs-gamecapture
+slug: ${__GAME_NAME__}
+version: Installer
+wine:
+  battleye: true
+  dxvk_nvapi: false
+  eac: true
+  fsr: false
+  vkd3d: false
+system:
+  prefix_command: ${HOME}/.var/app/net.lutris.Lutris/data/lutris/runners/wine/obs-gamecapture
+EOF
+else
+cat > $HOME/.var/app/net.lutris.Lutris/data/lutris/games/${__GAME_NAME__}-0.yml << EOF
+game:
+		exe: $HOME/Games/${__GAME_NAME__}/drive_c/Program Files (x86)/Battle.net/Battle.net Launcher.exe
+  prefix: $HOME/Games/${__GAME_NAME__}/
+game_slug: ${__GAME_NAME__}
+name: ${__GAME_NAME__}
+script:
+  game:
+    exe: $HOME/Games/${__GAME_NAME__}/drive_c/Program Files (x86)/Battle.net/Battle.net Launcher.exe
+    prefix: $HOME/Games/${__GAME_NAME__}/
+  wine:
+    battleye: true
+    dxvk_nvapi: false
+    eac: true
+    fsr: false
+    vkd3d: false
+slug: ${__GAME_NAME__}
+version: Installer
+wine:
+  battleye: true
+  dxvk_nvapi: false
+  eac: true
+  fsr: false
+  vkd3d: false
+EOF
+fi
+
+flatpak run net.lutris.Lutris "lutris:rungameid/${__GAME_ID__}"
