8ea6ba36e025b6fd7f29f2e4f01efcdf54eef46c
TaYaKi71751 <shantian.tw@gmail.com>
2024-10-12 01:12:29 +0900

    Find at home & which

--- sh/tailscale/up.sh
+++ sh/tailscale/up.sh
@@ -8,26 +8,11 @@ done
 
 ORIG_HOME=${HOME}
 
-# https://github.com/ValveSoftware/SteamOS/issues/1039
-function check_kdialog(){
-	export KDIALOG_USABLE=$(find / -name 'kdialog' -type f -exec {} --help \;)
-	export KDIALOG_USABLE="$(echo $KDIALOG_USABLE | grep Usage)"
-}
-
-function check_zenity(){
-	export ZENITY_USABLE=`find / -name 'zenity' -type f -exec {} --help \;`
-	export ZENITY_USABLE="$(echo $ZENITY_USABLE | grep Usage)"
-	env | grep STEAM_DECK\= && unset $ZENITY_USABLE
-}
-
-check_kdialog
-check_zenity
-
 function get_password(){
-	if [ -n "${kdialog_usable}" ];then
-		find / -name 'kdialog' -type f -exec bash -c "{} --password 'enter password' && pkill find " \;
-	elif [ -n "${zenity_usable}" ];then
-		find / -name 'zenity' -type f -exec bash -c "{} --password && pkill find"
+	if ( which kdialog );then
+		kdialog --password 'Enter password'
+	elif ( which zenity );then
+		zenity --password
 	fi
 }
 
@@ -48,7 +33,7 @@ check_sudo
 
 
 function auto_path() {
-	TARGET_PATHS="$(find / -name "$1" -type f)"
+	TARGET_PATHS="$(find $HOME -name "$1" -type f)"
 	echo $TARGET_PATHS
 	while IFS= read -r line
 	do
