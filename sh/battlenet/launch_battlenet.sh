ada21d95551f3e696015f65aa374a803819dbf83
TaYaKi71751 <shantian.tw@gmail.com>
2025-01-17 21:09:15 +0900

    Just use a battlenet-standard

--- sh/battlenet/launch_battlenet.sh
+++ sh/battlenet/launch_battlenet.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
 
-mkdir -p ${HOME}/BattleNet/
-cd ${HOME}/BattleNet
-
-__GAME_NAME__="battlenet"
-
-mkdir -p "$HOME/Games/${__GAME_NAME__}/drive_c/"
-
-# rm $HOME/.var/app/net.lutris.Lutris/data/lutris/pga.db
-__GAME_ID__="$(sqlite3 $HOME/.var/app/net.lutris.Lutris/data/lutris/pga.db << EOF
-SELECT id FROM games WHERE gameslug = '${__GAME_NAME__}';
-EOF
-)"
-
-flatpak run net.lutris.Lutris "lutris:rungameid/${__GAME_ID__}"
+flatpak run net.lutris.Lutris "lutris:battlenet-standard"
