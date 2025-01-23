9230ff4c56ce51a63ce53c47cb6a278e98ac4d60
TaYaKi71751 <shantian.tw@gmail.com>
2025-01-23 17:59:22 +0900

    Use aur & Add launch.sh

--- sh/curseforge/install.sh
+++ sh/curseforge/install.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-cd $HOME
-mkdir -p CurseForge
-cd CurseForge
-rm curseforge-latest-linux.zip
-rm -rf curseforge-latest-linux
-wget https://curseforge.overwolf.com/downloads/curseforge-latest-linux.zip
-unzip curseforge-latest-linux.zip
+yay -S curseforge --noconfirm
