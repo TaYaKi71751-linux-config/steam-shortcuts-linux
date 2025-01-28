92e2d756390d23f6d633d4efcfcdea8ea91159c3
TaYaKi71751 <shantian.tw@gmail.com>
2025-01-28 20:21:38 +0900

    Use arch mirror when x86_64

--- sh/discord/install_discord.sh
+++ sh/discord/install_discord.sh
@@ -1,4 +1,50 @@
 #!/bin/bash
+# https://github.com/ValveSoftware/SteamOS/issues/1039
+function check_kdialog(){
+	export KDIALOG_USABLE=$(find / -name 'kdialog' -type f -exec {} --help \;)
+	export KDIALOG_USABLE="$(echo $KDIALOG_USABLE | grep Usage)"
+}
+
+function check_zenity(){
+	export ZENITY_USABLE=`find / -name 'zenity' -type f -exec {} --help \;`
+	export ZENITY_USABLE="$(echo $ZENITY_USABLE | grep Usage)"
+	env | grep STEAM_DECK\= && unset $ZENITY_USABLE
+}
+check_kdialog
+check_zenity
+
+function get_password(){
+	if [ -n "${KDIALOG_USABLE}" ];then
+		find / -name 'kdialog' -type f -exec bash -c "{} --password 'Enter Password' && pkill find " \;
+	elif [ -n "${ZENITY_USABLE}" ];then
+		find / -name 'zenity' -type f -exec bash -c "{} --password && pkill find"
+	fi
+}
+
+
+function check_sudo() {
+	if ( `sudo -nv` );then
+		return "0"
+	fi
+
+	export SUDO_PASSWORD=$(get_password)
+	# https://github.com/SteamDeckHomebrew/decky-installer/releases/latest/download/user_install_script.sh…
+	if ( echo ${SUDO_PASSWORD} | sudo -S echo A | grep A );then
+		export SUDO_PASSWORD=${SUDO_PASSWORD}
+	else
+		check_sudo
+	fi
+}
+check_sudo
+
+# https://superuser.com/questions/553932/how-to-check-if-i-have-sudo-access
+function sudo_executor(){
+	if ( `sudo -nv` );then
+		sudo $@
+	else
+		echo ${SUDO_PASSWORD} | sudo -S $@
+	fi
+}
 
 function user_install_flatpak_package(){
 	PACKAGE_NAME="$1"
@@ -42,7 +88,13 @@ fi
 
 }
 
-
 #Discord
-system_install_flatpak_package com.discordapp.Discord
-user_install_flatpak_package com.discordapp.Discord
+if ( uname -a | grep x86_64 );then
+	mkdir -p "${HOME}/Discord"
+	rm "${HOME}/Discord/discord.pkg.tar.zst"
+	curl -LsSf https://archlinux.org/packages/extra/x86_64/discord/download/ -o "${HOME}/Discord/discord.pkg.tar.zst"
+	sudo_executor pacman -U "${HOME}/Discord/discord.pkg.tar.zst"
+else
+	system_install_flatpak_package com.discordapp.Discord
+	user_install_flatpak_package com.discordapp.Discord
+fi
--- sh/discord/launch_discord.sh
+++ sh/discord/launch_discord.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-/usr/bin/flatpak "run" "--branch=stable" "--file-forwarding" "com.discordapp.Discord" "@@u" "@@"
+#Discord
+if ( uname -a | grep x86_64 );then
+	if ( which discord );then
+		discord
+	else
+		/usr/bin/flatpak "run" "--branch=stable" "--file-forwarding" "com.discordapp.Discord" "@@u" "@@"
+	fi
+else
+	/usr/bin/flatpak "run" "--branch=stable" "--file-forwarding" "com.discordapp.Discord" "@@u" "@@"
+fi
