ade3fe9906ad5a1cdcafd381547e3faf719f821b
TaYaKi71751 <shantian.tw@gmail.com>
2025-04-07 15:13:12 +0900

    Use symlink

--- sh/opentaiko/install.sh
+++ sh/opentaiko/install.sh
@@ -3,5 +3,6 @@
 sudo pacman -Syu gstreamer gst-plugins-base gst-plugins-good gst-plugins-bad gst-plugins-ugly gst-libav --noconfirm --overwrite '*'
 yay -Sy gst-plugin-fdk_aac --noconfirm --overwrite '*'
 yay -Sy opentaikohub-bin --noconfirm --overwrite '*'
-sudo mkdir -p /usr/lib/OpenTaiko-Hub/
+mkdir -p $HOME/OpenTaiko-Hub
+sudo ln -sf $HOME/OpenTaiko-Hub /usr/lib/
 sudo chown $USER:$USER -R /usr/lib/OpenTaiko-Hub/
