61be74a2354d924f5bcc6fd5eb386d586919c32b
TaYaKi71751 <shantian.tw@gmail.com>
2025-04-07 14:27:23 +0900

    Fix cd

--- sh/opentaiko/install.sh
+++ sh/opentaiko/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir -p $HOME/opentaiko
-cd opentaiko
+cd $HOME/opentaiko
 rm OpenTaiko.Linux.x64.zip
 curl -LsSf https://github.com/0auBSQ/OpenTaiko/releases/latest/download/OpenTaiko.Linux.x64.zip -o OpenTaiko.Linux.x64.zip > /dev/null 2>&1
 rm -rf publish
