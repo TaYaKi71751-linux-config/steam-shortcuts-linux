ff47ed69bb33b77b0e29a51a36b89fe434998b4e
TaYaKi71751 <shantian.tw@gmail.com>
2025-04-07 15:05:42 +0900

    Remove check

--- sh/opentaiko/install.sh
+++ sh/opentaiko/install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 sudo pacman -Syu gstreamer gst-plugins-base gst-plugins-good gst-plugins-bad gst-plugins-ugly gst-libav --noconfirm --overwrite '*'
-gst-inspect-1.0 autoaudiosink
 yay -Sy gst-plugin-fdk_aac --noconfirm --overwrite '*'
-gst-inspect-1.0 fdkaacdec
 yay -Sy opentaikohub-bin --noconfirm --overwrite '*'
