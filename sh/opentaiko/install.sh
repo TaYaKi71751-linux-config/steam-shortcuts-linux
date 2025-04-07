4ed259b5a3a4d7dd86e13d2c0935ebb79d55f475
TaYaKi71751 <shantian.tw@gmail.com>
2025-04-07 15:08:45 +0900

    Add directory for OpneTaiko-Hub

--- sh/opentaiko/install.sh
+++ sh/opentaiko/install.sh
@@ -3,3 +3,5 @@
 sudo pacman -Syu gstreamer gst-plugins-base gst-plugins-good gst-plugins-bad gst-plugins-ugly gst-libav --noconfirm --overwrite '*'
 yay -Sy gst-plugin-fdk_aac --noconfirm --overwrite '*'
 yay -Sy opentaikohub-bin --noconfirm --overwrite '*'
+sudo mkdir -p /usr/lib/OpenTaiko-Hub/
+sudo chown $USER:$USER -R /usr/lib/OpenTaiko-Hub/
