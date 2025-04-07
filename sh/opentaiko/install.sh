552bf04b4cd359d54372a325578d4073a4be1ab7
TaYaKi71751 <shantian.tw@gmail.com>
2025-04-07 15:03:14 +0900

    Add dependencies

--- sh/opentaiko/install.sh
+++ sh/opentaiko/install.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+sudo pacman -Syu gstreamer gst-plugins-base gst-plugins-good gst-plugins-bad gst-plugins-ugly gst-libav --noconfirm --overwrite '*'
+gst-inspect-1.0 autoaudiosink
+yay -Sy gst-plugin-fdk_aac --noconfirm --overwrite '*'
+gst-inspect-1.0 fdkaacdec
 yay -Sy opentaikohub-bin --noconfirm --overwrite '*'
