421252564dd62a0dc177cac202e27133444233a1
TaYaKi71751 <shantian.tw@gmail.com>
2025-04-14 14:29:57 +0900

    Drop OpenTaiko

--- sh/opentaiko/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo pacman -Syu gstreamer gst-plugins-base gst-plugins-good gst-plugins-bad gst-plugins-ugly gst-libav --noconfirm --overwrite '*'
-yay -Sy gst-plugin-fdk_aac --noconfirm --overwrite '*'
-yay -Sy opentaikohub-bin --noconfirm --overwrite '*'
-mkdir -p $HOME/OpenTaiko-Hub
-sudo ln -sf $HOME/OpenTaiko-Hub /usr/lib/
-sudo chown $USER:$USER -R /usr/lib/OpenTaiko-Hub/
