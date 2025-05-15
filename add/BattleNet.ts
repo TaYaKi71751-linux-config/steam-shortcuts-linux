9af0f89d792e19529868e7bac801ea6337eb7677
TaYaKi71751 <shantian.tw@gmail.com>
2025-05-15 19:39:23 +0900

    Use proton_experimental for battlenet

--- add/BattleNet.ts
+++ add/BattleNet.ts
@@ -11,10 +11,11 @@ const __GAME_NAME__ = 'Battle.net';
 const __OUT_NAME__ = 'battlenet';
 const outPath = path.join(`${process.env.PWD}`, 'out', __OUT_NAME__);
 let apps = [
-	{AppName: '[Lutris] Install Battle.net', exe: path.join(outPath, 'install_battlenet.out'), StartDir: outPath,},
-	{AppName: '[Lutris] Battle.net', exe: path.join(outPath, 'launch_battlenet.out'), StartDir: outPath,},
-	{AppName: '[Lutris] Launch Diablo II Resurrected', exe: path.join(outPath, 'launch_d2r.out'), StartDir: outPath,},
-	{AppName: '[Lutris] World of Warcraft', exe: path.join(outPath, 'launch_wow.out'), StartDir: outPath, background: `${process.env.PWD}/image/background/wow.png`, wideCapsule: `${process.env.PWD}/image/widecapsule/wow.png`, capsule: `${process.env.PWD}/image/capsule/wow.png`, logo: `${process.env.PWD}/image/logo/wow.png`, icon: `${process.env.PWD}/image/icon/wow.png` },
+	{AppName: '[Lutris] Install Battle.net', exe: path.join(outPath, 'install_battlenet.out'), StartDir: outPath, LaunchOptions: '%command%'},
+	{AppName: '[Lutris] Battle.net', exe: path.join(outPath, 'launch_battlenet.out'), StartDir: outPath,LaunchOptions:'%command%'},
+	[AppName: '[Proton] Battle.net', exe: path.join(process.env.HOME,'Games','battlenet','drive_c','Program Files (x86)','Battle.net','Battle.net.exe'), StartDir: path.join(process.env.HOME,'Games','battlenet','drive_c','Program Files (x86)','Battle.net'), compat:'proton_experimental', LaunchOptions:`STEAM_COMPAT_DATA_PATH="${process.env.HOME}/Games/battlenet" %command%` },
+	{AppName: '[Lutris] Launch Diablo II Resurrected', exe: path.join(outPath, 'launch_d2r.out'), StartDir: outPath,LaunchOptions:'%command%'},
+	{AppName: '[Lutris] World of Warcraft', exe: path.join(outPath, 'launch_wow.out'), StartDir: outPath, background: `${process.env.PWD}/image/background/wow.png`, wideCapsule: `${process.env.PWD}/image/widecapsule/wow.png`, capsule: `${process.env.PWD}/image/capsule/wow.png`, logo: `${process.env.PWD}/image/logo/wow.png`, icon: `${process.env.PWD}/image/icon/wow.png`,LaunchOptions: '%command%' },
 ];
 
 export async function __main__ () {
@@ -23,9 +24,15 @@ export async function __main__ () {
 	{
 		const tags = [__GAME_NAME__,'Lutris'];
 		for (let i = 0; i < apps?.length; i++) {
-			const { AppName, exe, StartDir, icon, background, wideCapsule, capsule, logo } = apps[i];
+			const { compat, AppName, exe, StartDir, icon, background, wideCapsule, capsule, logo, LaunchOptions } = apps[i];
 			const appid = getShortcutAppID({ AppName, exe });
-			AddShortcut({ appid, AppName, exe, StartDir, icon: icon ?? '' });
+			AddShortcut({ appid, AppName, exe, StartDir, icon: icon ?? '', LaunchOptions });
+			if(compat){
+				AddCompat({
+					appid,
+					compat: compat,
+				});
+			}
 			if (background){
 				setBackground({
 					appid,
