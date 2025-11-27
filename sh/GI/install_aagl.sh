862a5a5e425df62817faffb51bf19a9fdb81045b
TaYaKi71751 <shantian.tw@gmail.com>
2025-11-27 22:57:04 +0900

    Install latest org.gnome.Platform

--- sh/GI/install_aagl.sh
+++ sh/GI/install_aagl.sh
@@ -41,11 +41,55 @@ fi
 
 }
 
+function system_install_latest_flatpak_package(){
+	PACKAGE_NAME="$1"
+RESULT="$(bash << EOF
+flatpak install --system ${PACKAGE_NAME} --assumeyes
+EOF
+)"
+if ( echo ${RESULT} | grep already\ installed );then
+	echo A
+	return
+elif ( echo ${RESULT} | grep ${PACKAGE_NAME} | grep Similar );then
+	LATEST_LIST="$(echo "${RESULT}" | grep "[" | grep "]" | cut -d '-' f1 | rev | cut -d ']' | rev)"
+	PACKAGE_LINES="$(echo "${RESULT}" | grep "${PACKAGE_NAME}/" | grep "${LATEST_LIST})")"
+while IFS= read -r line
+do
+bash << EOF
+	flatpak install --system $(echo $line | rev | cut -d ' ' -f1 | rev) --assumeyes
+EOF
+done < <(printf '%s\n' "$PACKAGE_LINES")
+fi
+
+}
+
+function user_install_latest_flatpak_package(){
+	PACKAGE_NAME="$1"
+RESULT="$(bash << EOF
+flatpak install --user ${PACKAGE_NAME} --assumeyes
+EOF
+)"
+if ( echo ${RESULT} | grep already\ installed );then
+	echo A
+	return
+elif ( echo ${RESULT} | grep ${PACKAGE_NAME} | grep Similar );then
+	LATEST_LIST="$(echo "${RESULT}" | grep "[" | grep "]" | cut -d '-' f1 | rev | cut -d ']' | rev)"
+	PACKAGE_LINES="$(echo "${RESULT}" | grep "${PACKAGE_NAME}/" | grep "${LATEST_LIST})")"
+while IFS= read -r line
+do
+bash << EOF
+	flatpak install --user $(echo $line | rev | cut -d ' ' -f1 | rev) --assumeyes
+EOF
+done < <(printf '%s\n' "$PACKAGE_LINES")
+fi
+
+}
+
 # Install instructions by
 # https://github.com/an-anime-team/an-anime-game-launcher/wiki/Installation#-any-distribution-flatpak
 
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak remote-add --if-not-exists --user launcher.moe https://gol.launcher.moe/gol.launcher.moe.flatpakrepo
 
-system_install_flatpak_package org.gnome.Platform//47
-user_install_flatpak_package moe.launcher.an-anime-game-launcher
+system_install_latest_flatpak_package org.gnome.Platform//49
+user_install_latest_flatpak_package moe.launcher.an-anime-game-launcher
