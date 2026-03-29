1c6a946c125ec296bc5cb2474957580eedd24ff6
TaYaKi71751 <shantian.tw@gmail.com>
2026-03-30 06:19:11 +0900

    Fix Railway.ts

--- add/Railway.ts
+++ add/Railway.ts
@@ -4,28 +4,27 @@ import { getShortcutAppID } from '../util/AppID';
 import { AddShortcut, RemoveShortcutStartsWith } from '../util/Shortcut';
 import { AddToCats } from '../util/Categories';
 import { AddCompat } from '../util/Compatibilities';
-import path,{ dirname } from 'path';
+import path, { dirname } from 'path';
 
 export async function __main__ () {
-
 	// AAGL
 	{
 		let tags = ['Honkai StarRail', 'The Honkers Railway Launcher'];
 
-		const outPath = path.join(`${process.env.PWD}`,'out','HS');
-		const outFiles = ['install_railway.out','launch_railway.out'];
+		const outPath = path.join(`${process.env.PWD}`, 'out', 'HS');
+		const outFiles = ['install_railway.out', 'launch_railway.out'];
 		for (let i = 0; i < outFiles?.length; i++) {
 			const filename = outFiles[i];
 			const StartDir = outPath;
 			const exe = path.join(outPath, filename);
-			const AppName = '[Railway]' + (function () {
+			const AppName = '' + (function () {
 				switch (filename) {
-					case 'install_railway.out':
-						tags = ['Install', 'Honkai StarRail'];
-					return 'Install';
-					case 'launch_railway.out':
-						tags = ['Honkai StarRail'];
-					return 'Honkai StarRail Launcher';
+				case 'install_railway.out':
+					tags = ['Install', 'Honkai StarRail'];
+					return 'Install Honkai: Star Rail';
+				case 'launch_railway.out':
+					tags = ['Honkai StarRail'];
+					return 'Honkai: Star Rail';
 				}
 			})();
 			const appid = getShortcutAppID({ AppName, exe });
